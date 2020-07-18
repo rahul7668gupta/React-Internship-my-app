b7a77447a93ac1cf6fcfd491bb0a64e761715081
rahul7668gupta <rahul7668gupta@gmail.com>
2020-07-18 16:59:54 +0530

    Updates

---
+++ precache-manifest.3c91c5ff0d24635073cb15f4c11df893.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97d442a7acfe76297433e901b1b6738b",
+    "url": "/React-Internship-my-app/index.html"
+  },
+  {
+    "revision": "92e8566d179e3e2bbb38",
+    "url": "/React-Internship-my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "12cc6ec73ea2ff095956",
+    "url": "/React-Internship-my-app/static/js/2.d2f996e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Internship-my-app/static/js/2.d2f996e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92e8566d179e3e2bbb38",
+    "url": "/React-Internship-my-app/static/js/main.b7923b98.chunk.js"
+  },
+  {
+    "revision": "ba003f20d464056ba72b",
+    "url": "/React-Internship-my-app/static/js/runtime-main.98f17bf1.js"
+  }
+]);
