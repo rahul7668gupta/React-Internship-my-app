4c84b5a347ab2c784c6820638075b4ed92a5ce9e
rahul7668gupta <rahul7668gupta@gmail.com>
2020-07-20 12:16:57 +0530

    Succesfully input box  using hooks

--- src/StatesInFunction.js
+++ src/StatesInFunction.js
@@ -2,12 +2,16 @@ import React, { useState } from "react";
 
 let StatesInFunction = () => {
     let [count, setCount] = useState(0);
+    let handleChange = (event)=>{
+        setCount(event.target.value);
+    }
     return (
         <div>
             <h2>
                 {count}
             </h2>
             <br />
+            <input type="number" onChange={handleChange}></input>
             <button onClick={() => setCount(count+=1)}>Increment</button><br />
             <button onClick={() => setCount(count -= 1)}>Decrement</button><br />
             <button onClick={() => setCount(0)}>Reset</button>
